9573a3a4716983779b9073d2b5c54698a1d43ae1
kamranfrompakistan <103025361+kamranfrompakistan@users.noreply.github.com>
2022-04-11 11:05:42 +0500

    Add files via upload

---
+++ DigitalStopWatch/Program.cs
@@ -0,0 +1,35 @@
+﻿using Model;
+
+namespace StopWatchConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string? Option;
+            bool ExitProgram;
+            DigitalStopWatch StopWatch = new DigitalStopWatch();
+
+            ExitProgram = false;
+
+            while (!ExitProgram)
+            {
+                Console.WriteLine("Enter your choice 1 for start, 2 for stop, any other key for exit");
+                Option = Console.ReadLine().Trim();
+
+                switch (Option)
+                {
+                    case "1":
+                        StopWatch.Start();
+                        break;
+                    case "2":
+                        StopWatch.Stop();
+                        break;
+                    default:
+                        ExitProgram = true;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Model/DigitalStopWatch.cs
@@ -0,0 +1,48 @@
+﻿namespace Model
+{
+    public class DigitalStopWatch
+    {
+        public bool IsRunning { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public DigitalStopWatch()
+        {
+            IsRunning = false;
+            Console.WriteLine("----------------- Digital Stop Watch ------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------");
+            Console.WriteLine("");
+        }
+        public bool Start()
+        {
+            if (IsRunning)
+            {
+                Console.WriteLine("Stopwatch is already running...");
+                return false;
+            }
+            IsRunning = true;
+            StartTime = DateTime.Now;
+            Console.WriteLine("Stopwatch started...");
+            return true;
+        }
+
+        public bool Stop()
+        {
+            if (!IsRunning)
+            {
+                Console.WriteLine("Stopwatch is not running...");
+                return false;
+            }
+            IsRunning = false;
+            EndTime = DateTime.Now;
+            Console.WriteLine("Stopwatch is stopped...");
+            Console.WriteLine("");
+            Console.WriteLine("Time Elapsed:");
+            Console.WriteLine(EndTime.ToUniversalTime() - StartTime.ToUniversalTime());
+            Console.WriteLine("");
+            Console.WriteLine("");
+            return true;
+        }
+
+
+    }
+}
